afc061ab9722d66bc9f3dbc63d4c029500aa0173
haizhiyaoji <olefin@sina.com>
2019-12-20 17:46:54 +0900

    3 files modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -8,6 +8,7 @@ import { CartService } from '../cart.service';
 })
 export class ShippingComponent implements OnInit {
   shippingCosts;
+  
   constructor(
     private cartService: CartService
   ) {
